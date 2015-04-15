31f68a20832280587e120a5a1e0fc8d08a639286
gklayza <gerson.kieffer@gmail.com>
2015-04-15 18:52:57 -0500

    primer commit

---
+++ app/src/main/java/com/example/usuario/adapters/GridViewActivity.java
@@ -0,0 +1,68 @@
+﻿package com.example.usuario.adapters;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ *
+ 
+
+*/
+
+public class GridViewActivity extends ActionBarActivity
+{
+
+   
+ String[] mDatos;
+  
+  GridView mGridView;
+    
+TextView mTextView;
+
+   
+ @Override
+  
+  protected void onCreate(Bundle savedInstanceState) {
+   
+     super.onCreate(savedInstanceState);
+       
+ setContentView(R.layout.gridview_layout);
+
+
+    
+    mDatos = getResources().getStringArray(R.array.datos);
+       
+ mTextView = (TextView) findViewById(R.id.textView);
+       
+ mGridView = (GridView) findViewById(R.id.gridView);
+
+
+/* GridView */
+     
+ ArrayAdapter<String> mMensaje = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDatos);
+      
+  mGridView.setAdapter(mMensaje);
+/* Escucha Items GridView */
+   
+     mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+
+ public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+ 
+ String seleccionadoString = getResources().getString(R.string.select) + mDatos[position];
+  
+ Toast.makeText(getApplicationContext(), seleccionadoString, Toast.LENGTH_SHORT).show();
+  
+          }
+        });
+    }
+}
